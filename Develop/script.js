90048857828b6417176376d4004754a324efc413
libbyou <lcyou95@gmail.com>
2023-09-30 16:18:03 -0500

    javascript code added

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,63 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//console.log(numArray);
+
+var alphaArray = "abcdefghijklmnopqrstuvwxyz".split("");
+//console.log(alphaArray);
+
+var upperAlphaArray = alphaArray.map(function (x) {
+  return x.toUpperCase();
+});
+//console.log(upperAlphaArray);
+
+var charArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+//console.log(charArray);
+
+
 // Write password to the #password input
 function writePassword() {
   var numOfChars = window.prompt("How many characters would you like your password to be?");
   if (!numOfChars) {
     return;
   }
-  numOfChars=parseFloat(numOfChars);
-  console.log(numOfChars);
+  numOfChars = parseInt(numOfChars);
+  //console.log(numOfChars);
 
-  var charArrayChoice = window.alert("Do you want to include special characters in your password?");
-  if (charArrayChoice===true) {
-    var chars = Math.floor(Math.random() * charArray.length);
-    var randomChars = charArray[chars];
-  } else { 
+  var choices = [];
 
+  var charArrayChoice = window.confirm("Do you want to include special characters in your password?");
+  if (charArrayChoice === true) {
+    
+    choices.push(...charArray);
+  };
+
+  var upperAlphaArrayChoice = window.confirm("Do you want to include uppercase letters in your password?");
+  if (upperAlphaArrayChoice === true) {
+    choices.push(...upperAlphaArray);
+  };
+  
+  choices.push(...numArray);
+  
+  choices.push(...alphaArray);
+  //console.log(choices);
+
+  var password = [];
+  //console.log(password);
+  
+  for (let i = 0; i <numOfChars; i++) {
+    var randomInterger = Math.floor(Math.random() * choices.length);
+    password.push(choices[randomInterger]);
   }
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var passwordString = password.join('');
+  console.log(passwordString);
+
+  //var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+
+  passwordText.value=passwordString;
 
 }
 
@@ -28,35 +65,22 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // function generatePassword(){
-  // All your code here
+// All your code here
 // var numChar = window.prompt
 // console.log(numChar);
 // var inclNums = window.confirm true/false
 
 // array for each character type
-var numArray = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
-console.log (numArray);
-
-var alphaArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
-console.log(alphaArray);
-
-upperAlphaArray = alphaArray.map(function(x){
-  return x.toUpperCase();
-});
-console.log(upperAlphaArray);
-
-charArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"],
-console.log(charArray);
 
 // if statement
 // var options =[];
-  // if true push/concat that array into a new array var
+// if true push/concat that array into a new array var
 
 // for loop through options array based on required characters
-  // Random index value for every loop
-  // Google this; How to randomly choose a value from an array
-  // var chosenArr = [];
-  // turn array into string
+// Random index value for every loop
+// Google this; How to randomly choose a value from an array
+// var chosenArr = [];
+// turn array into string
 
-  //return password;
+//return password;
 //}
